cf212d73c805edb232d077566ffa049485b05108
apex2801 <aapinheiro2005@gmail.com>
2021-03-07 18:55:10 -0300

    finalizar o programa

---
+++ Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace musica_em_c_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("arthur albuquerque pinheiro");
+            Console.WriteLine("turma:1l3");
+            Console.WriteLine("musica em c#");
+            Console.WriteLine("-----------------");
+            Console.WriteLine("wesley safadão");
+            Console.WriteLine("-----------------");
+            Console.WriteLine("bora");
+           
+            Console.WriteLine("-----------------");
+            
+            Console.WriteLine("E se eu te encurralar no meu barraco tu vai ficar é louca");
+            Console.WriteLine("Louca de prazer, o couro vai comer");
+            Console.WriteLine("Se você deitar na minha cama eu vou te arrepiar");
+            Console.WriteLine("Bora se entregar, que fogo eu não nego");
+            Console.WriteLine("Também não arrego");
+            Console.ForegroundColor = ConsoleColor .Red;
+            Console.WriteLine("-----------------");
+            Console.WriteLine("Bora, bora, bora, bora");
+            Console.WriteLine("Umbora se entrosar");
+            Console.WriteLine("Bora, bora, bora, bora");
+            Console.WriteLine("Umbora se esfregar");
+            Console.WriteLine("Umbora, umbora, umbora, umbora");
+            Console.WriteLine("Se acabar, neném");
+            Console.WriteLine("Nesse rala e rola");
+            Console.ResetColor();
+            Console.WriteLine("-----------------");
+            Console.WriteLine("Se eu te encurralar no meu barraco tu vai ficar é louca");
+            Console.WriteLine("Louca de prazer, o couro vai comer");
+            Console.WriteLine("Se você deitar na minha cama eu vou te arrepiar");
+            Console.WriteLine("Bora se entregar, que fogo eu não nego");
+            Console.WriteLine("E também não arrego");
+            Console.ForegroundColor = ConsoleColor .Red;
+            Console.WriteLine("-----------------");
+            Console.WriteLine("Bora, bora, bora, bora");
+            Console.WriteLine("Umbora se entrosar");
+            Console.WriteLine("Bora, bora, bora, bora");
+            Console.WriteLine("E bora se esfregar");
+            Console.WriteLine("Umbora, umbora, umbora, umbora");
+            Console.WriteLine("Se acabar, neném");
+            Console.WriteLine("Nesse rala e rola");
+        }
+    }
+}
